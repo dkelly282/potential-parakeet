faebdec2b0f15a5b4d10250cdf9c5bf4c25ba007
dkelly282 <91381746+dkelly282@users.noreply.github.com>
2023-07-20 12:29:13 +0100

    Update my_pda.py

--- my_pda.py
+++ my_pda.py
@@ -1,6 +1,6 @@
 import csv
 print("List of family birthdays:")
-with open('Birthdays and age.csv','r') as data:
+with open('birthdays_and_age.csv','r') as data:
     reader = csv.reader(data)
     for row in reader:
         print (row)
