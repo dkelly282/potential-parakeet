35e63abf3625eb2b9243c3a9619a3ab2a9f8046f
dkelly282 <91381746+dkelly282@users.noreply.github.com>
2023-07-21 17:53:07 +0100

    Add files via upload

--- my_pda.py
+++ my_pda.py
@@ -1,6 +1,11 @@
 import csv
+from datetime import datetime
+now = datetime.now()
+date = now.strftime("%d/%m/%Y")
+print(f"Hello David\nToday's date is {date}")
 print("List of family birthdays:")
 with open('birthdays_and_age.csv','r') as data:
     reader = csv.reader(data)
     for row in reader:
         print (row)
+    
