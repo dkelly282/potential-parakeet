28de53006c3db48548bbc1bf30bff4685ebcd455
dkelly282 <91381746+dkelly282@users.noreply.github.com>
2024-06-02 19:52:56 +0100

    Add files via upload

---
+++ name.py
@@ -0,0 +1,7 @@
+userName = input ("Hello. What is your name? ")
+if userName.lower() == "diane":
+    print (f"Welcome {userName}! You are my best friend!")    
+elif userName.lower() == "david":
+    print ("Greetings Master")
+else:
+    print ("\nGo away. I do not know you.")
